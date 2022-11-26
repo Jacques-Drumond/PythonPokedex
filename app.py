5a61d683b233f090f91d51a8fb27f0f9da88bb5f
Jacques-Drumond <jacques.junio@hotmail.com>
2022-11-26 10:14:28 -0300

    squece

--- app.py
+++ app.py
@@ -1,17 +1,38 @@
 from functions import getpokemoninfo, habilidades, tipo
+from tkinter import *
 
-pokemon = input("What's the Pokémon? \n")
+ws = Tk()
+ws.title("PythonGuides")
+ws.geometry('400x300')
+ws['bg'] = '#ffbf00'
 
-poke_info = getpokemoninfo(pokemon)
+def printValue():
+    global pokemon, tipo
+    pokemon = player_name.get()
+    poke_info = getpokemoninfo(pokemon)
+    print(poke_info)
+    tipo = tipo(poke_info)
+    abilities = ", ".join(habilidades(poke_info))
+    print(f"Type: {tipo.capitalize()}")
+    print(f"Abilities: {abilities.capitalize()}")
 
-tipo = tipo(poke_info)
+
+player_name = Entry(ws)
+player_name.pack(pady=30)
+
+Button(
+    ws,
+    text="Register Player", 
+    padx=10, 
+    pady=5,
+    command=printValue
+    ).pack()
+
+ws.mainloop()
 
 
 
-abilities = ", ".join(habilidades(poke_info))
 
-print(f"Type: {tipo.capitalize()}")
 
-print(f"Abilities: {abilities.capitalize()}")
 
 
